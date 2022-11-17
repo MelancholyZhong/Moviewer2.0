0f388e5a4e9d10a584393ee052997f1159d443f1
Yao Zhong <a26250@baobeiben.local>
2022-11-17 15:34:24 -0800

    implemented review CRUD operations on database

--- server/db_controllers/review-controllers.js
+++ server/db_controllers/review-controllers.js
@@ -1,4 +1,6 @@
 const mongoUtil = require("../mongoUtil");
+const { ObjectId } = require("mongodb");
+const { query } = require("express");
 
 const DUMMY_REVIEWS = [
   {
@@ -9,24 +11,70 @@ const DUMMY_REVIEWS = [
   },
 ];
 
-const createReview = (review) => {
-  return DUMMY_REVIEWS[0];
+const createReview = async (review) => {
+  const db = mongoUtil.getDB();
+  const reviews = await db.collection("reviews");
+  try {
+    const result = await reviews.insertOne(review);
+    return result.insertedId;
+  } catch (err) {
+    return err;
+  }
 };
 
-const getReviewByMovie = (movie) => {
-  return DUMMY_REVIEWS;
+const getReviewByMovie = async (movieId) => {
+  const db = mongoUtil.getDB();
+  const reviews = await db.collection("reviews");
+  const query = { movieId: movieId };
+  try {
+    const result = await reviews.find(query).toArray();
+    return result;
+  } catch (err) {
+    return err;
+  }
 };
 
-const getReviewById = (id) => {
-  return DUMMY_REVIEWS[0];
+const getReviewById = async (id) => {
+  const db = mongoUtil.getDB();
+  const reviews = await db.collection("reviews");
+  const query = { _id: ObjectId(id) };
+  try {
+    const result = await reviews.findOne(query);
+    return result;
+  } catch (err) {
+    return err;
+  }
 };
 
-const updateReviewById = (reviewId, review) => {
-  return DUMMY_REVIEWS[0];
+const updateReviewById = async (reviewId, content) => {
+  const db = mongoUtil.getDB();
+  const reviews = await db.collection("reviews");
+
+  try {
+    const filter = { _id: ObjectId(reviewId) };
+    const updateDocument = {
+      $set: {
+        content: content,
+      },
+    };
+    await reviews.updateOne(filter, updateDocument);
+    const result = reviews.findOne(filter);
+    return result;
+  } catch (err) {
+    return err;
+  }
 };
 
-const deleteReviewById = (id) => {
-  return `deleted one review from ${DUMMY_REVIEWS[0].userId}`;
+const deleteReviewById = async (id) => {
+  const db = mongoUtil.getDB();
+  const reviews = await db.collection("reviews");
+  try {
+    const query = { _id: ObjectId(id) };
+    const result = await reviews.deleteOne(query);
+    return `delete ${result.deletedCount} review`;
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = {
